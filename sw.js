089a4b9214d7c1f6fb447cfe65ff835ef51626e8
turgutaydin1 <turgutaydin1@gmail.com>
2026-08-26 17:06:16 +0300

    Yeni ders ve görsel sürümü için önbelleği yenile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='siber-hazirlik-v1-20260826';
+const CACHE='siber-hazirlik-v2-20260826';
 const A=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});

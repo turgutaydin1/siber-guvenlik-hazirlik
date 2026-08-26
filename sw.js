7c661efa828588efe82803d1db60b644944a1f65
turgutaydin1 <turgutaydin1@gmail.com>
2026-08-26 17:21:59 +0300

    Yeni üniversite dersleri sürümü için önbelleği yenile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='siber-hazirlik-v2-20260826';
+const CACHE='siber-hazirlik-v3-20260826';
 const A=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
